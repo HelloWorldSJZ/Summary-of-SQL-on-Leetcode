a0b0073572966b9797c6543195b6b4ab8f0b1721
HelloWorldSJZ <91241158+HelloWorldSJZ@users.noreply.github.com>
2022-07-31 22:41:25 +0800

    Update 182. Duplicate Emails.sql
    和586. Customer Placing the Largest Number of Orders类似，底层逻辑都是找一列中重复的行。
    可以用join or having count

--- Basics/182. Duplicate Emails.sql
+++ Basics/182. Duplicate Emails.sql
@@ -1,6 +1,10 @@
-{
 SELECT DISTINCT a.Email FROM Person a 
 JOIN Person b
  ON (a.Email = b.Email)
  WHERE a.Id <> b.Id
-}
+ -- 找出表格a版中不重复的email
+ -- 以id为合并key
+ -- 选出a版中和b版id不一样但是重复的email
+ 
+ SELECT email FROM person group by email having count(*) > 1
+ -- having count用于分组后过滤的条件
