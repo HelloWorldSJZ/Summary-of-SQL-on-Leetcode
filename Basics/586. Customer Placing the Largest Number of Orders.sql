b673a80ab6df8f1a14252d7d4bd41bc3e1000c54
HelloWorldSJZ <91241158+HelloWorldSJZ@users.noreply.github.com>
2022-07-31 22:48:38 +0800

    Update 586. Customer Placing the Largest Number of Orders.sql

--- Basics/586. Customer Placing the Largest Number of Orders.sql
+++ Basics/586. Customer Placing the Largest Number of Orders.sql
@@ -8,3 +8,4 @@ group by customer_number
 having count(order_number) = (select count(*) as count from orders group by customer_number order by count desc limit 1);
 -- 找max值，用having count() = blabla
 -- order by在select之后
+-- 找最值方法，降序排列 找第一
