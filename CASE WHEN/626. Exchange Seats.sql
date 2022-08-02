1853f498fc99ea555250edb8b68d0487cc0d886c
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-08-02 22:24:22 +0800

    Create 626. Exchange Seats.sql

---
+++ CASE WHEN/626. Exchange Seats.sql
@@ -0,0 +1,9 @@
+select  case when id = (select max(id) from seat) and id%2 != 0 then id
+             when id%2 = 1 then id + 1 
+             else id - 1
+        end as id,
+        student 
+from seat 
+order by id
+
+-- 换名字可以转换思路换id！！！
