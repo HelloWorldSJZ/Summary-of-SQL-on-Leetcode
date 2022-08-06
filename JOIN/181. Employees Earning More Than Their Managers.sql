df8229c6406155db56178e941df7e1101a02b6f6
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-08-06 21:23:29 +0800

    Update 181. Employees Earning More Than Their Managers.sql

--- JOIN/181. Employees Earning More Than Their Managers.sql
+++ JOIN/181. Employees Earning More Than Their Managers.sql
@@ -2,3 +2,8 @@ select a.name as Employee from employee a
 join employee b 
 on a.managerid = b.id               -- people have managers
 where a.salary > b.salary           -- employee whose salary is higher 
+
+
+select E1.Name 
+from Employee as E1, Employee as E2 
+where E1.ManagerId = E2.Id and E1.Salary > E2.Salary
