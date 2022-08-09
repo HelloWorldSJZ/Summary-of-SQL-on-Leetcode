b742b5f086668196092ee7be211af49ec36b7c1d
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-08-09 22:25:21 +0800

    Update 607. Sales Person.sql

--- Subquery/607. Sales Person.sql
+++ Subquery/607. Sales Person.sql
@@ -5,3 +5,15 @@ where s.sales_id not in
         (select c.com_id from company c where name = "RED")
     )
         
+
+
+Select o.sales_id from orders o where o.com_id = 
+        (select c.com_id from company c where name = "RED")
+        
+==
+
+SELECT sales_id FROM orders
+LEFT JOIN
+company 
+ON orders.com_id=company.com_id 
+WHERE company.name='RED'
