98264e9861ea40723c8cbaf7ea6282b1e6cb8ddb
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-10-26 18:02:47 -0400

    Update 512. Game Play Analysis II.sql

--- Subquery/512. Game Play Analysis II.sql
+++ Subquery/512. Game Play Analysis II.sql
@@ -1,3 +1,4 @@
+# Subquery
 select player_id, device_id 
 from Activity  
 where (player_id, event_date) in 
@@ -12,8 +13,8 @@ select distinct player_id, first_value(device_id)
                   as device_id
                   from activity
                  
-
-
+                 
+                 
 
 # Rank
 SELECT player_id
@@ -26,3 +27,13 @@ FROM
 WHERE rnk_table.rnk = 1
 先给table的每个group进行排序，然后选出每个group第一行的id
 
+
+
+
+# Join
+select t1.player_id, t1.device_id from activity t1 
+    join (select player_id, min(event_date) event_date from Activity
+       group by player_id) t2 
+       on t1.player_id = t2.player_id and t1.event_date = t2.event_date
+       
+ 先从原table中选出两个id，join已经选出的最早的id和date，同时满足id相等，最早日期
