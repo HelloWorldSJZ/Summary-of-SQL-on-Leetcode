ca93126f23deb7747dd885bee637ffd84445d53f
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2022-10-26 17:31:05 -0400

    Update 512. Game Play Analysis II.sql

--- Subquery/512. Game Play Analysis II.sql
+++ Subquery/512. Game Play Analysis II.sql
@@ -3,3 +3,26 @@ from Activity
 where (player_id, event_date) in 
        (select player_id, min(event_date) from Activity
        group by player_id)
+当player id和event date是最早的时候选出对应的device id
+
+
+# Window function 
+select distinct player_id, first_value(device_id) 
+                  over (partition by player_id order by event_date) 
+                  as device_id
+                  from activity
+                 
+
+
+
+# Rank
+SELECT player_id
+        ,device_id
+FROM 
+    (SELECT player_id
+            ,device_id
+            ,rank() OVER(PARTITION BY player_id ORDER BY event_date) AS rnk
+    FROM Activity) rnk_table
+WHERE rnk_table.rnk = 1
+先给table的每个group进行排序，然后选出每个group第一行的id
+
