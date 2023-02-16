119bfc9a4ec2ffc0242d40c14a4893ea4073579f
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-02-16 10:37:34 -0500

    Update 570. Managers with at Least 5 Direct Reports.sql

--- Subquery/570. Managers with at Least 5 Direct Reports.sql
+++ Subquery/570. Managers with at Least 5 Direct Reports.sql
@@ -3,3 +3,7 @@ where id in
 (select managerId from employee
 group by managerId
 having count(managerId) >= 5)
+
+
+# 用表格的不同列之间的联系选出另一个value
+# select ... from ... where id in (subquery选出另一个符合条件的id list)
