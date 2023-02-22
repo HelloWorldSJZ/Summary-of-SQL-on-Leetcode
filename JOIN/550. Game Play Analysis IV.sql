e351c22b4dad00c49b8c31df7cd5d1bbf84ff7fe
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-02-22 10:50:14 -0500

    Update 550. Game Play Analysis IV.sql

--- JOIN/550. Game Play Analysis IV.sql
+++ JOIN/550. Game Play Analysis IV.sql
@@ -11,6 +11,6 @@ SELECT
     
 SELECT ROUND(COUNT(t2.player_id)/COUNT(t1.player_id),2) AS fraction
 FROM 
-(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) t1 
-LEFT JOIN Activity t2
+(SELECT player_id, MIN(event_date) AS first_login FROM Activity GROUP BY player_id) t1 # 选出每个player的first login date
+LEFT JOIN Activity t2                                                                  # 用join filer出连续两天login的id
 ON t1.player_id = t2.player_id AND t1.first_login = t2.event_date - 1
