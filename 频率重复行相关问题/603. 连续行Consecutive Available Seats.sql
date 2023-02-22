a42dcf14f056509d6b0d648c90df629228a9e32f
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-02-22 12:04:36 -0500

    Update 603. 连续行Consecutive Available Seats.sql

--- 频率重复行相关问题/603. 连续行Consecutive Available Seats.sql
+++ 频率重复行相关问题/603. 连续行Consecutive Available Seats.sql
@@ -4,6 +4,9 @@ where free = 1 and
 or
 seat_id + 1 in (select seat_id from cinema where free = 1))
 
+
+
+# 先self join成五个表，每个表里面筛选同时满足连续和free条件的id
 select distinct a.seat_id
 from cinema a join cinema b
   on abs(a.seat_id - b.seat_id) = 1
