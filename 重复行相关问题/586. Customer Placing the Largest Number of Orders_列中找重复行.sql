44483d6a4678ed02c6676c10bf5f659edcc05614
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-02-22 11:06:54 -0500

    Update 586. Customer Placing the Largest Number of Orders_列中找重复行.sql

--- 重复行相关问题/586. Customer Placing the Largest Number of Orders_列中找重复行.sql
+++ 重复行相关问题/586. Customer Placing the Largest Number of Orders_列中找重复行.sql
@@ -9,3 +9,9 @@ having count(order_number) = (select count(*) as count from orders group by cust
 -- 找max值，用having count() = blabla
 -- order by在select之后
 -- 找最值方法，降序排列 找第一
+
+
+select customer_number from (select customer_number, count(customer_number) count 
+from orders group by customer_number 
+order by count desc
+limit 1) t1
