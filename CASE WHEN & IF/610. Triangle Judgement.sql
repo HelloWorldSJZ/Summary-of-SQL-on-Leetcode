755675e2cfac3caa1b3744e9e2d505ec532bcbd8
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-02-23 10:19:02 -0500

    Update 610. Triangle Judgement.sql

--- CASE WHEN & IF/610. Triangle Judgement.sql
+++ CASE WHEN & IF/610. Triangle Judgement.sql
@@ -1,1 +1,7 @@
 select x,y,z,if(x+y>z and x+z>y and z+y>x, "Yes","No") as triangle from triangle 
+
+
+select x,y,z, case when x+y>z and x+z>y and z+y>x then 'Yes'
+else 'No'
+end as triangle
+from triangle 
