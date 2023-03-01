2815686ea09fa3b7077de61ae07b5cb206a65dbf
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-03-01 16:27:20 -0500

    Update 1076. Project Employees II.sql

--- having/1076. Project Employees II.sql
+++ having/1076. Project Employees II.sql
@@ -1,8 +1,17 @@
 # max出现在了不同的id里
 select project_id from project 
 group by project_id
-having(count(project_id)) = (select count(project_id) count
-    from project 
+having count(employee_id) = (select count(employee_id) countfrom project 
     group by project_id 
     order by count desc
     limit 1)
+
+
+
+select project_id
+from
+(select project_id, 
+        rank() over(order by count(employee_id) desc) as rk 
+from Project
+group by project_id) t
+where rk = 1
