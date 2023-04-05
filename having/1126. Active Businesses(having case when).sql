7d9fd4ebcd029443a85c347b780449c4076ec0e4
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-04-05 00:06:55 -0400

    Update 1126. Active Businesses(having case when).sql

--- having/1126. Active Businesses(having case when).sql
+++ having/1126. Active Businesses(having case when).sql
@@ -8,3 +8,12 @@ group by business_id
 having sum(CASE WHEN occurences > mean THEN 1 ELSE 0 END) > 1
 
 # having SUM(IF(occurences > mean,1,0)) > 1
+
+
+# sub-sub-subquery
+
+select business_id from (
+    select *, if(occurences>mean,1,0) as act_if from (
+        select *, avg(occurences) over (partition by event_type) as mean from events) t) t1
+group by business_id
+having sum(act_if) > 1
