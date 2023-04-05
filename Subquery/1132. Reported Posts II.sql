3acdbcc982744f406679f03c1b6c8773bb3a563e
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-04-05 12:15:49 -0400

    Create 1132. Reported Posts II.sql

---
+++ Subquery/1132. Reported Posts II.sql
@@ -0,0 +1,11 @@
+
+select round(avg(percent),2) as average_daily_percent
+from
+    (select a.action_date,
+    count(distinct r.post_id)/count(distinct a.post_id)*100 as percent
+    from actions a left join removals r
+    on a.post_id = r.post_id
+    where a.extra='spam'
+    group by 1) temp;
+    
+    注意题目要求
