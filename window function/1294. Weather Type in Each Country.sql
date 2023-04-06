4de3fb7dd994fb9f5ade16652c419235180d2477
JingshuZ <91241158+mmmzzss@users.noreply.github.com>
2023-04-05 23:59:54 -0400

    Update 1294. Weather Type in Each Country.sql

--- window function/1294. Weather Type in Each Country.sql
+++ window function/1294. Weather Type in Each Country.sql
@@ -1,11 +1,14 @@
+
+with cte as ((select country_name, 
+       avg(weather_state) over (partition by w.country_id, date_format(day, '%Y-%m')) as avg 
+from weather w
+left join countries c
+on w.country_id = c.country_id
+where date_format(day, '%Y-%m') = '2019-11'))
+
 select distinct country_name, 
        case when avg <=15 then 'Cold'
        when avg >= 25 then 'Hot'
        else 'Warm'
        end as weather_type
-from (select country_name, 
-       avg(weather_state) over (partition by w.country_id, date_format(day, '%Y-%m')) as avg 
-from weather w
-left join countries c
-on w.country_id = c.country_id
-where date_format(day, '%Y-%m') = '2019-11') t
+from cte 
